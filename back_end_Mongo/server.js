abb7ad834758833265c895faefc74372ed04bfc8
James Jewitt <jamestjewitt@gmail.com>
2022-09-01 23:03:26 -0400

    Both db's init w/ Express servers

--- back_end_Mongo/server.js
+++ back_end_Mongo/server.js
@@ -1,12 +1,14 @@
 const express=require('express')
 const cors=require('cors')
 const logger=require('morgan')
-const PORT=process.env.PORT||3001
+const PORTMONGO=process.env.PORT||3001
 const db=require('./db')
 const app=express()
 
 app.use(cors())
 app.use(express.json())
 app.use(logger('dev'))
-app.get('/',(req,res)=>{res.send('Hit mongo!')})
-app.listen(PORT,()=>{console.log(`Express server (mongo) listening on port: ${PORT}`)})
+app.get('/mongo',(req,res)=>{res.send('Hit Mongo!')})
+app.listen(PORTMONGO,()=>{console.log(
+    `Express server paired with Mongo listening on port: ${PORTMONGO}`)})
+ 
--- back_end_SQL/server.js
+++ back_end_SQL/server.js
@@ -3,11 +3,14 @@ const cors=require('cors')
 const app=express()
 
 const AppRouter=require('./routes/AppRouter')
-const PORT=process.env.PORT||3002
+const PORTSQL=process.env.PORT||3002
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.get('/',(req,res)=>res.json({message:'SQL server hit!'}))
 app.use('/api',AppRouter)
-app.listen(PORT,()=>console.log(`Server (SQL) started on port:${PORT}`))
+app.listen(PORTSQL,()=>console.log(
+    `Express server paired with SQL started on port: ${PORTSQL}`))
+
+
