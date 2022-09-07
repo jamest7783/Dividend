4dd2859cf93786cc31af85d3b09607e9ca157d17
James Jewitt <jamestjewitt@gmail.com>
2022-09-07 14:22:22 -0400

    Equity getHistorical w/ period param

--- back_end_SQL/controllers/equity.js
+++ back_end_SQL/controllers/equity.js
@@ -1,6 +1,17 @@
+const yahooFinance=require('yahoo-finance')
 const {Equity}=require('../models')
 
+const getHistorical=async (req,res)=>{
+    const {ticker,period}=req.body
+    const equity=await Equity.findOrCreate({where:{ticker}})
+    const to=new Date().toISOString().slice(0, 10)
+    const from=(parseInt(to.substring(0,4))-1).toString()+to.substring(4,to.length)
+    const history=await yahooFinance.historical({symbol:equity[0].ticker,from,to,period})
+    res.status(200).json(history)
+}
+
 module.exports={
+    getHistorical
 }
 
 
--- back_end_SQL/routes/index.js
+++ back_end_SQL/routes/index.js
@@ -7,6 +7,9 @@ router.get('/portfolio/read/:pk',portfolio.readPortfolio)
 router.put('/portfolio/update/:pk',portfolio.updatePortfolio)
 router.delete('/portfolio/delete/:pk',portfolio.deletePortfolio)
 router.get('/portfolio/read/:pk/positions',portfolio.readPortfolioPositions)
+
 router.post('/order/create',order.createOrder)
+
+router.post('/equity/historical',equity.getHistorical)
 
 module.exports=router
