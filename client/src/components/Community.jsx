758800dde843acce9c53bb81128fbcca6b1b447f
James Jewitt <jamestjewitt@gmail.com>
2022-09-09 01:09:54 -0400

    Community draft

--- client/src/components/Community.jsx
+++ client/src/components/Community.jsx
@@ -2,7 +2,33 @@ const Community=()=>{
 
     return(
         <div id='glass'>
-            community
+            <div id='trending'>
+                <div className='trending-tile'></div>
+                <div className='trending-tile'></div>
+                <div className='trending-tile'></div>
+                <div className='trending-tile'></div>
+            </div>
+            <div id='thread-container'>
+                <div id='threads'>
+                    <div id='thread'></div>
+                    <div id='thread'></div>
+                    <div id='thread'></div>
+                    <div id='thread'></div>
+                    <div id='thread'></div>
+                    <div id='thread'></div>
+                    <div id='thread'></div>
+                    <div id='thread'></div>
+                    <div id='thread'></div>
+                    <div id='thread'></div>
+                    <div id='thread'></div>
+                </div>
+                <div id='top-performers'>
+                    <div className='top-performer'></div>
+                    <div className='top-performer'></div>
+                    <div className='top-performer'></div>
+                </div>
+            </div>
+
         </div>
     )
 }
