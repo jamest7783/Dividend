61192df4c520583932457f8584e0c7bb1d78e091
James Jewitt <jamestjewitt@gmail.com>
2022-09-11 00:14:42 -0400

    Fill news tiles with query

--- client/src/components/News.jsx
+++ client/src/components/News.jsx
@@ -1,13 +1,38 @@
+import {useEffect,useState} from 'react'
+import axios from 'axios'
+
 const News=()=>{
+
+    const [query,setQuery]=useState('')
+    const [news,setNews]=useState([])
+    const handleChange=(e)=>{setQuery({query:e.target.value})}
+    const requestNews=async ( )=>{
+        const res=await axios.post('http://localhost:3001/api/watchlist/news',query)
+        setNews(res.data.items)
+    }
+
 
     return(
         <div id='glass'>
             <div id='news-search-container'>
-                <button id='news-search-button'>search</button>
-                <input id='news-search-input'></input>
+                <button id='news-search-button'
+                    onClick={(e)=>{requestNews()}}>
+                    search
+                </button>
+                <input id='news-search-input'          
+                    onChange={handleChange}
+                    name='search'
+                    type='search'
+                    placeholder='search'
+                ></input>
             </div>
             <div id='news'>
-
+                {news?.map((article)=>(
+                    <div id='tile'>
+                        <div>{article.title}</div>
+                        <div>{article.link}</div>
+                    </div>
+                ))}
             </div>
         </div>
     )
